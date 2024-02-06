b7179e6ebe04fd3339c6ed65fa55b569004f7c84
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-06 20:46:04 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+// Rekürsif metot
+static int FibonacciSerisi(int sayi)
+{
+    if (sayi == 0)
+    {
+        return 0;
+    }
+    else if(sayi == 1){
+        return 1;
+    }
+    else
+    {
+        return FibonacciSerisi(sayi - 1) + FibonacciSerisi(sayi - 2);
+    }
+}
+
+//Extension metot
+public static class MyExtensionClass
+{
+    public static bool IsEventNumber(this int value)
+    {
+        if (value % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+}
